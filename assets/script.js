5509cf8e184c44001157852aefec2ba927f6c19c
Alina Badillo <alinachristabel108@gmail.com>
2023-07-14 08:55:39 -0500

    Added another function to use the long and lat from input city and display other parameters

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
-var APIkey = 'fcafd102401c66de2b3db010da96e87c';
 var currentDate = dayjs().format("(MM/DD/YYYY)");
 var dateToday = document.querySelector(".dateToday");
 dateToday.innerHTML = currentDate;
 var searchBtn = document.querySelector('#searchBtn');
+//Will add later for local storage
 var cityHistory = [];
 
 
-
-function getGeo(e) {
-  event.preventDefault()
+function getGeo(event) {
+  event.preventDefault();
   var inputField = document.getElementById("inputField").value;
   console.log(inputField);
 
@@ -16,7 +15,24 @@ function getGeo(e) {
   fetch(requestGeoUrl)
     .then(function (response) {
       console.log(response);
-      return response.json();l
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      console.log(lat,lon);
+      getWeather(lat,lon);
+    })
+};
+
+function getWeather(lat,lon) {
+  var requestWeatherUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&units=imperial&appid=fcafd102401c66de2b3db010da96e87c';
+  console.log(requestWeatherUrl);
+  fetch(requestWeatherUrl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
     })
     .then(function (data) {
       console.log(data);
@@ -24,3 +40,4 @@ function getGeo(e) {
 }
 
 searchBtn.addEventListener("click", getGeo);
+
