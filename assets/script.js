96cc1851e9c410c68c069b0998dca43197085546
Alina Badillo <alinachristabel108@gmail.com>
2023-07-13 22:14:56 -0500

    Redid script.js a little bit, added working geo API and concatenated it with searched city

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
-var APIkey = 'f0a9523b71afb17bdad1a28e540e4425';
+var APIkey = 'fcafd102401c66de2b3db010da96e87c';
+var currentDate = dayjs().format("(MM/DD/YYYY)");
+var dateToday = document.querySelector(".dateToday");
+dateToday.innerHTML = currentDate;
+var searchBtn = document.querySelector('#searchBtn');
+var cityHistory = [];
+
+
+
+function getGeo(e) {
+  event.preventDefault()
+  var inputField = document.getElementById("inputField").value;
+  console.log(inputField);
+
+  var requestGeoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputField + '&limit=1&appid=fcafd102401c66de2b3db010da96e87c';
+  fetch(requestGeoUrl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();l
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+}
+
+searchBtn.addEventListener("click", getGeo);
